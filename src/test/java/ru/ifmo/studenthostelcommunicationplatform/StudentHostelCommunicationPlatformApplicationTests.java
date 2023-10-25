5025c347536c0bb99117953a8b708fc0248784b0
ITerNik <ternik2705@list.ru>
2023-10-25 16:26:43 +0300

    Structured project directories

---
+++ src/test/java/ru/ifmo/studenthostelcommunicationplatform/StudentHostelCommunicationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.studenthostelcommunicationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHostelCommunicationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
