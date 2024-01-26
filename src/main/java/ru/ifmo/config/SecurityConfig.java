18a623e0af75ce90b38e42878ebd99ec0a5d8f2c
ITerNik <ternik2705@list.ru>
2024-01-26 10:52:53 +0300

    Added jwt token managing and edited security configuration

---
+++ src/main/java/ru/ifmo/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package ru.ifmo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.ifmo.security.JWTFilter;
+import ru.ifmo.services.CredentialsService;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("ru.ifmo.security")
+public class SecurityConfig {
+    private final JWTFilter jwtFilter;
+    private final CredentialsService credentialsService;
+
+    @Autowired
+    public SecurityConfig(JWTFilter jwtFilter, CredentialsService credentialsService) {
+        this.jwtFilter = jwtFilter;
+        this.credentialsService = credentialsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(credentialsService)
+                .passwordEncoder(passwordEncoder());
+        return builder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
